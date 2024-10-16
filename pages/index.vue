9e76d62318d3a9c6d4ebec8355d41f18357da613
Eric Hoover <ehoover3@yahoo.com>
2024-10-16 14:28:48 -0400

    add spinner ui component

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <MainSection title="Home" :loading="loading">
+      <div>Home</div>
+
+      <div class="border-b" :class="twitterBorderColor">Message Form</div>
+
+      <TweetListFeed :tweets="homeTweets" />
+    </MainSection>
+  </div>
+</template>
+<script setup>
+const { twitterBorderColor } = useTailwindConfig();
+c;
+</script>
