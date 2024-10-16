5f70180fb92adacb3fe8319852875bd5300d34e5
Eric Hoover <ehoover3@yahoo.com>
2024-10-16 11:53:15 -0400

    add a placeholder logo

--- app.vue
+++ app.vue
@@ -6,7 +6,9 @@
         <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
           <!-- Left sidebar -->
           <div class="hidden md:block xs-col-span-1 xl:col-span-2">
-            <div class="sticky top-0">Left Sidebar</div>
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
           </div>
 
           <!-- Main content -->
