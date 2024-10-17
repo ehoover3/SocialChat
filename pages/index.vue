c08c0371e9eeb51054966948a7b56cc229a5d4b8
Eric Hoover <ehoover3@yahoo.com>
2024-10-16 23:54:42 -0400

    update main section index file

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <MainSection title="Home" :loading="loading">
-      <div>Home</div>
+      <Head>
+        <Title>Home / Twitter</Title>
+      </Head>
 
-      <div class="border-b" :class="twitterBorderColor">Message Form</div>
+      <div class="border-b" :class="twitterBorderColor">
+        <TweetForm :user="user" @on-success="handleFormSuccess" />
+      </div>
 
       <TweetListFeed :tweets="homeTweets" />
     </MainSection>
@@ -11,4 +15,30 @@
 </template>
 <script setup>
 const { twitterBorderColor } = useTailwindConfig();
+const { getTweets } = useTweets();
+
+const loading = ref(false);
+const homeTweets = ref([]);
+const { useAuthUser } = useAuth();
+
+const user = useAuthUser();
+
+onBeforeMount(async () => {
+  loading.value = true;
+  try {
+    const { tweets } = await getTweets();
+
+    homeTweets.value = tweets;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loading.value = false;
+  }
+});
+
+function handleFormSuccess(tweet) {
+  navigateTo({
+    path: `/status/${tweet.id}`,
+  });
+}
 </script>
