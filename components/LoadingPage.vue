642fd0bab9444aaa91e2fd253bfb01178c00ed84
Eric Hoover <ehoover3@yahoo.com>
2024-10-29 23:50:17 -0400

    Breaking changes to pivot to an aws tutorial app, since the previous app did not successfully deploy on aws.  My goal is to successfully deploy, then incorporate the previous app code into this

--- components/LoadingPage.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="flex items-center justify-center h-screen">
-    <div class="absolute w-20 h-20 animate-ping">
-      <LogoSocialMedia />
-    </div>
-  </div>
-</template>
-<script setup></script>
