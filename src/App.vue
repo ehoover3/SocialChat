2971f078c2da22021e2c41614a5588e2ca1d8247
Eric Hoover <ehoover3@yahoo.com>
2024-10-31 22:36:27 -0400

    update app.vue layout

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,45 @@
 <!-- src/App.vue -->
 <script setup lang="ts">
+import { ref } from "vue";
 import { Authenticator } from "@aws-amplify/ui-vue";
 import "@aws-amplify/ui-vue/styles.css";
 import SidebarLeft from "./components/SidebarLeft.vue";
 import SidebarRight from "./components/SidebarRight.vue";
 import Main from "./components/Main.vue";
+const darkMode = ref(true);
 </script>
 
 <template>
-  <authenticator>
-    <template v-slot="{ user, signOut }">
-      <div class="app-layout">
-        <SidebarLeft @signOut="signOut" />
-        <Main :user="user" @signOut="signOut" />
-        <SidebarRight />
-      </div>
-    </template>
-  </authenticator>
+  <div :class="{ dark: darkMode }">
+    <div class="bg-red dark:bg-dim-900">
+      <authenticator>
+        <template v-slot="{ user, signOut }">
+          <div class="app-layout">
+            <div class="hidden md:block xs-col-span-1 xl:col-span-2">
+              <div class="sticky top-0">
+                <SidebarLeft @signOut="signOut" />
+              </div>
+            </div>
+
+            <main class="col-span-12 md:col-span-8 xl:col-span-6 pb-20 md:pb-0">
+              <Main :user="user" @signOut="signOut" />
+            </main>
+
+            <div class="hidden col-span-12 md:block xl:col-span-4 md:col-span-3">
+              <div class="sticky top-0">
+                <SidebarRight />
+              </div>
+            </div>
+          </div>
+        </template>
+      </authenticator>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .app-layout {
   display: flex;
   min-height: 100vh;
-  font-family: Arial, sans-serif;
-}
-
-.sidebar-left,
-.sidebar-right {
-  flex-shrink: 0;
-  background-color: #f4f4f9;
 }
 </style>
