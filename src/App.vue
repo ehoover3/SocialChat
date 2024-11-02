222de7564401421ed10f81d1be98ff22ef3b41a6
Eric Hoover <ehoover3@yahoo.com>
2024-11-01 21:13:20 -0400

    set up tailwind format

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,40 @@ const darkMode = ref(true);
 
 <template>
   <div :class="{ dark: darkMode }">
+    <div class="bg-white dark:bg-dim-900">
+      <authenticator>
+        <template v-slot="{ user, signOut }">
+          <div class="min-h-full">
+            <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+              <div class="hidden md:block xs-col-span-1 xl:col-span-2">
+                <div class="sticky top-0">
+                  <SidebarLeft @signOut="signOut" />
+                </div>
+              </div>
+
+              <div class="md:hidden fixed bottom-0 left-0 right-0 p-4 bg-gray-800 text-white text-center">
+                <button class="bg-blue-500 px-4 py-2 rounded-full">Tweet</button>
+              </div>
+
+              <main class="col-span-12 md:col-span-8 xl:col-span-6 pb-20 md:pb-0">
+                <Main />
+              </main>
+
+              <div class="hidden col-span-12 md:block xl:col-span-4 md:col-span-3">
+                <div class="sticky top-0">
+                  <SidebarRight />
+                </div>
+              </div>
+            </div>
+          </div>
+        </template>
+      </authenticator>
+    </div>
+  </div>
+</template>
+
+<!-- <template>
+  <div :class="{ dark: darkMode }">
     <div class="bg-red dark:bg-dim-900">
       <authenticator>
         <template v-slot="{ user, signOut }">
@@ -36,11 +70,4 @@ const darkMode = ref(true);
       </authenticator>
     </div>
   </div>
-</template>
-
-<style scoped>
-.app-layout {
-  display: flex;
-  min-height: 100vh;
-}
-</style>
+</template> -->
