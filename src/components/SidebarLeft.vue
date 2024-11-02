776c6286d190b16c943047f41176b9acfaa3d96e
Eric Hoover <ehoover3@yahoo.com>
2024-11-02 13:57:26 -0400

    update sidebar left with icons and text

--- src/components/SidebarLeft.vue
+++ src/components/SidebarLeft.vue
@@ -1,17 +1,51 @@
 <!-- src/components/SidebarLeft.vue -->
 <template>
-  <aside class="w-full p-5 bg-gray-100 dark:bg-gray-800 min-h-screen text-gray-800 dark:text-gray-100 shadow-lg">
-    <h2 class="text-xl font-bold mb-4">Menu</h2>
-    <ul class="space-y-2">
-      <li><a href="#" class="block p-2 hover:bg-pink-300 dark:hover:bg-pink-700 transition rounded">Home</a></li>
-      <li><a href="#" class="block p-2 hover:bg-pink-300 dark:hover:bg-pink-700 transition rounded">Profile</a></li>
-      <li><a href="#" class="block p-2 hover:bg-pink-300 dark:hover:bg-pink-700 transition rounded">Settings</a></li>
-    </ul>
-    <button @click="handleSignOut" class="mt-6 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 dark:hover:bg-blue-400 transition">Sign Out</button>
-  </aside>
+  <div class="flex flex-col h-screen">
+    <div class="p-2 my-2 rounded-full hover:bg-blue-50 w-min dark:hover:bg-white/20">
+      <div class="w-8 h-8">
+        <!-- <LogoSocialMedia /> -->
+      </div>
+    </div>
+
+    <div class="mt-2 space-y-3">
+      <SidebarLeftTab title="Home">
+        <template v-slot:icon>
+          <HomeIcon />
+        </template>
+        <template v-slot:name> Home </template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab to="/profile" title="Profile">
+        <template v-slot:icon>
+          <UserIcon />
+        </template>
+        <template v-slot:name> Profile </template>
+      </SidebarLeftTab>
+
+      <button class="flex items-center p-3 text-black rounded-full w-min hover:bg-gray-200 dark:hover:bg-dim-200 dark:text-white" @click="handleSignOut">Sign Out</button>
+
+      <!-- <div class="hidden xl:block" title="Log Out" @click="handleLogout">
+        <UIButton liquid size="lg">
+          <span class="font-bold"> Log Out </span>
+        </UIButton>
+      </div> -->
+
+      <!-- <div class="block xl:hidden" title="Log Out" @click="handleLogout">
+        <UIButton @on-click="emits('onTweet')">
+          <div class="w-6 h-6 font-bold">
+            <LogoutIcon />
+          </div>
+        </UIButton>
+      </div> -->
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { HomeIcon, UserIcon } from "@heroicons/vue/solid";
+import { LogoutIcon } from "@heroicons/vue/outline";
+import SidebarLeftTab from "./SidebarLeftTab.vue";
+
 import { defineProps, defineEmits } from "vue";
 
 const props = defineProps<{ user: Record<string, any> }>();
