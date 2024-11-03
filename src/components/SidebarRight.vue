4a035015a1fb89ae6a5784dd52c80cefc11a059d
Eric Hoover <ehoover3@yahoo.com>
2024-11-03 00:20:28 -0400

    update profile page

--- src/components/SidebarRight.vue
+++ src/components/SidebarRight.vue
@@ -1,7 +1,6 @@
 <!-- src/components/SidebarRight.vue -->
 <template>
   <div class="w-full p-4 bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-100 rounded-lg shadow-md min-h-screen">
-    <h2 class="text-xl font-bold mb-4">Profile</h2>
     <a href="https://erichoover.org" target="_blank" rel="noopener noreferrer" class="flex items-center justify-center flex-col p-4 transition-transform transform hover:scale-105">
       <img class="w-24 h-24 rounded-full border-4 border-blue-500 shadow-md" :src="profileImage" alt="Profile" />
       <p class="mt-2 text-sm font-bold text-blue-600 dark:text-blue-400 cursor-pointer hover:underline">See My Full Portfolio</p>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,17 +1,29 @@
 <!-- src/components/Profile.vue -->
 <template>
   <main class="max-w-md mx-auto p-6 bg-white dark:bg-gray-900 rounded-lg shadow-md text-gray-800 dark:text-white">
-    <h2 class="text-2xl font-bold">Profile</h2>
+    <h2 class="text-3xl font-bold text-center mb-6">Profile</h2>
+    <p class="mb-4"><strong>Username:</strong> {{ user.username }}</p>
+    <p class="mb-4"><strong>Email:</strong> {{ user?.signInDetails?.loginId }}</p>
+
     <div>
-      <p><strong>Username:</strong> {{ user.username }}</p>
-      <p><strong>Email:</strong> {{ user.email }}</p>
-      <!-- Add other user details here -->
+      <form @submit.prevent="saveProfile">
+        <div class="mb-4">
+          <label class="block font-semibold" for="username">Username:</label>
+          <input id="username" v-model="user.username" type="text" class="w-full p-2 border rounded focus:outline-none focus:ring focus:ring-blue-500" required />
+        </div>
+
+        <div class="mb-4">
+          <label class="block font-semibold" for="email">Email:</label>
+          <input id="email" v-model="user.email" type="email" class="w-full p-2 border rounded focus:outline-none focus:ring focus:ring-blue-500" required />
+        </div>
+
+        <button type="submit" class="w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-200">Save Changes</button>
+      </form>
     </div>
   </main>
 </template>
 
 <script setup lang="ts">
 import { defineProps } from "vue";
-
 const props = defineProps<{ user: Record<string, any> }>();
 </script>
