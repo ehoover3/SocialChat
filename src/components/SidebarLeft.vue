b74f47147b2fd5ee2efa6c13ba291387096d3faa
Eric Hoover <ehoover3@yahoo.com>
2024-11-02 21:02:48 -0400

    update sidebar left icons

--- src/components/SidebarLeft.vue
+++ src/components/SidebarLeft.vue
@@ -1,9 +1,9 @@
 <!-- src/components/SidebarLeft.vue -->
 <template>
-  <div class="flex flex-col h-screen">
+  <div class="flex flex-col items-center h-screen">
     <div class="p-2 my-2 rounded-full hover:bg-blue-50 w-min dark:hover:bg-white/20">
       <div class="w-8 h-8">
-        <!-- <LogoSocialMedia /> -->
+        <LogoSocialMedia />
       </div>
     </div>
 
@@ -22,32 +22,21 @@
         <template v-slot:name> Profile </template>
       </SidebarLeftTab>
 
-      <button class="flex items-center p-3 text-black rounded-full w-min hover:bg-gray-200 dark:hover:bg-dim-200 dark:text-white" @click="handleSignOut">Sign Out</button>
-
-      <!-- <div class="hidden xl:block" title="Log Out" @click="handleLogout">
+      <div class="hidden xl:block" title="Log Out" @click="handleSignOut">
         <UIButton liquid size="lg">
           <span class="font-bold"> Log Out </span>
         </UIButton>
-      </div> -->
-
-      <!-- <div class="block xl:hidden" title="Log Out" @click="handleLogout">
-        <UIButton @on-click="emits('onTweet')">
-          <div class="w-6 h-6 font-bold">
-            <LogoutIcon />
-          </div>
-        </UIButton>
-      </div> -->
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { HomeIcon, UserIcon } from "@heroicons/vue/solid";
-import { LogoutIcon } from "@heroicons/vue/outline";
 import SidebarLeftTab from "./SidebarLeftTab.vue";
-
+import UIButton from "./UI/UIButton.vue";
 import { defineProps, defineEmits } from "vue";
-
+import LogoSocialMedia from "./LogoSocialMedia.vue";
 const props = defineProps<{ user: Record<string, any> }>();
 const emit = defineEmits();
 
--- src/components/UI/UIButton.vue
+++ src/components/UI/UIButton.vue
@@ -1,3 +1,5 @@
+<!-- src/components/UI/UIButton.vue -->
+
 <template>
   <button class="flex justify-center text-white bg-blue-400 rounded-full hover:bg-blue-500 font-sm disabled:bg-blue-300 disabled:cursor-not-allowed" :disabled="props.disabled" :class="classes" @click="handleClick">
     <span :class="textFontSize">
@@ -5,7 +7,8 @@
     </span>
   </button>
 </template>
-<script setup>
+
+<!-- <script setup>
 const emits = defineEmits(["onClick"]);
 
 const props = defineProps({
@@ -20,6 +23,59 @@ const props = defineProps({
   liquid: {
     type: Boolean,
     deafult: false,
+  },
+});
+
+const paddingClasses = computed(() => {
+  switch (props.size) {
+    case "sm":
+      return "py-2 px-3";
+    case "lg":
+      return "px-4 py-3";
+    default:
+      return "px-3 py-3";
+  }
+});
+
+const textFontSize = computed(() => {
+  switch (props.size) {
+    case "lg":
+      return "text-md";
+    default:
+      return "text-sm";
+  }
+});
+
+const defaultWidth = computed(() => {
+  switch (props.size) {
+    default:
+      return "w-min";
+  }
+});
+
+const classes = computed(() => `${paddingClasses.value} ${props.liquid ? "w-full" : defaultWidth.value}`);
+
+function handleClick(event) {
+  emits("onClick", event);
+}
+</script> -->
+<script setup>
+import { computed } from "vue";
+
+const emits = defineEmits(["onClick"]);
+
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  size: {
+    type: String,
+    default: "md",
+  },
+  liquid: {
+    type: Boolean,
+    default: false,
   },
 });
 
