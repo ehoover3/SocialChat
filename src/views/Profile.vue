5173ba556953c73c5f9cf58208ed8a7fd953b944
Eric Hoover <ehoover3@yahoo.com>
2024-11-04 00:18:05 -0500

    remove unused profile page form

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,24 +2,8 @@
 <template>
   <main class="max-w-md mx-auto p-6 bg-white dark:bg-gray-900 rounded-lg shadow-md text-gray-800 dark:text-white">
     <h2 class="text-3xl font-bold text-center mb-6">Profile</h2>
-    <p class="mb-4"><strong>Username:</strong> {{ user.username }}</p>
+    <p class="mb-4"><strong>UserID:</strong> {{ user.username }}</p>
     <p class="mb-4"><strong>Email:</strong> {{ user?.signInDetails?.loginId }}</p>
-
-    <div>
-      <form @submit.prevent="saveProfile">
-        <div class="mb-4">
-          <label class="block font-semibold" for="username">Username:</label>
-          <!-- <input id="username" v-model="user.username" type="text" class="w-full p-2 border rounded focus:outline-none focus:ring focus:ring-blue-500" required /> -->
-        </div>
-
-        <div class="mb-4">
-          <label class="block font-semibold" for="email">Email:</label>
-          <!-- <input id="email" v-model="user.email" type="email" class="w-full p-2 border rounded focus:outline-none focus:ring focus:ring-blue-500" required /> -->
-        </div>
-
-        <button type="submit" class="w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-200">Save Changes</button>
-      </form>
-    </div>
   </main>
 </template>
 
