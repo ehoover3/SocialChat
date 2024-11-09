2b3d56a3ee4f5d4d4eb969629d0c2b733f721bb9
Eric Hoover <ehoover3@yahoo.com>
2024-11-08 23:28:12 -0500

    clean up sidebar left

--- src/components/SidebarLeft.vue
+++ src/components/SidebarLeft.vue
@@ -1,47 +1,38 @@
 <!-- src/components/SidebarLeft.vue -->
+
 <template>
   <aside class="rounded-lg m-2 mt-4 border border-gray-400 bg-gray-50 p-2">
     <div class="flex flex-col items-center">
       <div class="mt-2 space-y-3">
         <!-- Home Tab -->
-        <div class="flex items-center p-3 text-black rounded-full w-min hover:bg-gray-200" title="Home" @click="selectTab('home')">
-          <div class="w-6 h-6 text-dark">
-            <HomeIcon />
-          </div>
-          <div class="ml-4 text-xl hidden sm:block whitespace-nowrap">Home</div>
-        </div>
+        <button class="flex items-center p-3 text-black rounded-full w-min hover:bg-gray-200" title="Home" @click="selectTab('home')">
+          <HomeIcon class="w-6 h-6 text-dark" />
+          <span class="ml-4 text-xl hidden sm:block whitespace-nowrap">Home</span>
+        </button>
 
         <!-- Profile Tab -->
-        <div class="flex items-center p-3 text-black rounded-full w-min hover:bg-gray-200" title="Profile" @click="selectTab('profile')">
-          <div class="w-6 h-6 text-dark">
-            <UserIcon />
-          </div>
-          <div class="ml-4 text-xl hidden sm:block whitespace-nowrap">Profile</div>
-        </div>
+        <button class="flex items-center p-3 text-black rounded-full w-min hover:bg-gray-200" title="Home" @click="selectTab('home')">
+          <UserIcon class="w-6 h-6 text-dark" />
+          <span class="ml-4 text-xl hidden sm:block whitespace-nowrap">Profile</span>
+        </button>
 
         <!-- Privacy Policy Tab -->
-        <div class="flex items-center p-3 text-black rounded-full w-min hover:bg-gray-200" title="Privacy Policy" @click="selectTab('privacyPolicy')">
-          <div class="w-6 h-6 text-dark">
-            <IdentificationIcon />
-          </div>
-          <div class="ml-4 text-xl hidden sm:block whitespace-nowrap">Privacy Policy</div>
-        </div>
+        <button class="flex items-center p-3 text-black rounded-full w-min hover:bg-gray-200" title="Home" @click="selectTab('home')">
+          <IdentificationIcon class="w-6 h-6 text-dark" />
+          <span class="ml-4 text-xl hidden sm:block whitespace-nowrap">Privacy Policy</span>
+        </button>
 
         <!-- Cookie Policy Tab -->
-        <div class="flex items-center p-3 text-black rounded-full w-min hover:bg-gray-200" title="Cookie Policy" @click="selectTab('cookiePolicy')">
-          <div class="w-6 h-6 text-dark">
-            <FingerPrintIcon />
-          </div>
-          <div class="ml-4 text-xl hidden sm:block whitespace-nowrap">Cookie Policy</div>
-        </div>
+        <button class="flex items-center p-3 text-black rounded-full w-min hover:bg-gray-200" title="Home" @click="selectTab('home')">
+          <FingerPrintIcon class="w-6 h-6 text-dark" />
+          <span class="ml-4 text-xl hidden sm:block whitespace-nowrap">Cookie Policy</span>
+        </button>
 
         <!-- Terms of Service Tab -->
-        <div class="flex items-center p-3 text-black rounded-full w-min hover:bg-gray-200" title="Terms of Service" @click="selectTab('termsOfService')">
-          <div class="w-6 h-6 text-dark">
-            <ClipboardIcon />
-          </div>
-          <div class="ml-4 text-xl hidden sm:block whitespace-nowrap">Terms of Service</div>
-        </div>
+        <button class="flex items-center p-3 text-black rounded-full w-min hover:bg-gray-200" title="Home" @click="selectTab('home')">
+          <ClipboardIcon class="w-6 h-6 text-dark" />
+          <span class="ml-4 text-xl hidden sm:block whitespace-nowrap">Terms of Service</span>
+        </button>
 
         <!-- Log Out Button -->
         <div title="Log Out" @click="handleSignOut">
@@ -49,7 +40,6 @@
           <button class="w-full py-3 px-4 text-white rounded-full font-sm bg-blue-600 hover:bg-blue-700 disabled:bg-blue-300 disabled:cursor-not-allowed hidden sm:block">
             <span class="font-bold"> Log Out </span>
           </button>
-
           <!-- Mobile Screen Button -->
           <button class="w-full py-3 px-4 text-white rounded-full font-sm bg-blue-600 hover:bg-blue-700 disabled:bg-blue-300 disabled:cursor-not-allowed block sm:hidden">
             <span class="font-bold"> <LogoutIcon class="text-xl" /> </span>
