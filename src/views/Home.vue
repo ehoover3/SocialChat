7df4fe8d86a106845c6b4c008e72d1cadc6646ee
Eric Hoover <ehoover3@yahoo.com>
2024-11-10 09:12:03 -0500

    add URL to schema

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,7 +12,7 @@
       <li v-for="userMessage in userMessages" :key="userMessage.id" class="flex justify-between items-center p-4 border border-gray-400 rounded-lg bg-gray-50">
         <div class="flex flex-col w-full mr-4">
           <span v-if="!userMessage.isEditing && userMessage.content" class="font-medium text-gray-900">{{ userMessage.content }}</span>
-          <img v-else-if="!userMessage.isEditing && userMessage.url" :src="userMessage.url" alt="User Post Image" class="rounded-lg" />
+          <img v-if="!userMessage.isEditing && userMessage.url" :src="userMessage.url" alt="User Post Image" class="rounded-lg" />
 
           <textarea v-else v-model="userMessage.editedContent" class="py-1 px-2 border border-gray-300 rounded-lg w-full resize-y"></textarea>
 
@@ -86,6 +86,10 @@ function formatDate(dateString: string): string {
 function listPosts() {
   client.models.Todo.observeQuery().subscribe({
     next: ({ items, isSynced }) => {
+      items.forEach((item, index) => {
+        console.log(`Item ${index + 1}:`, item);
+      });
+
       userMessages.value = items
         .map((userMessage) => ({
           ...userMessage,
@@ -93,6 +97,7 @@ function listPosts() {
           email: userMessage.email || "No Email",
           isEditing: false,
           editedContent: userMessage.content,
+          url: userMessage.url || "No URL",
         }))
         .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()) as FormattedUserMessage[];
     },
